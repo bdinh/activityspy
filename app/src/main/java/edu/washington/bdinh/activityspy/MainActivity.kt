add8ca221c4e5d30c6391ab6174e4b694f57fed1
bdinh <baodinh96@gmail.com>
2018-04-12 13:29:57 -0700

    finished assignment

---
+++ app/src/main/java/edu/washington/bdinh/activityspy/MainActivity.kt
@@ -0,0 +1,22 @@
+package edu.washington.bdinh.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "onCreate event fired " + intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain! " + intent)
+    }
+
+}
